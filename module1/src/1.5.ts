6477bc3dd36eaf3b5dd78bbd088d7196cd28739f
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-11-04 10:27:18 +0600

    1-5 Object , Optional and Literal Types

---
+++ module1/src/1.5.ts
@@ -0,0 +1,42 @@
+{
+
+
+  // Object , Optional and Literal Types
+
+  const user: {
+    company: "Programming Hero"; // Types --->  Literal Types
+    readonly companyLocation: "Dhaka, Bangladesh"; 
+    firstName: string;
+    middleName?: string; // Optional Types
+    lastName: string;
+    isMarried: boolean;
+  } = {
+    company: "Programming Hero",
+    companyLocation: "Dhaka, Bangladesh",
+    firstName: "Mehedi",
+    lastName: "Mehad",
+    isMarried: true,
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //
+}
