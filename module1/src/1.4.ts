17d5e079c5af4e06f20ff2c89e5855243f0b7395
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-11-04 10:25:22 +0600

    1-4 Basic data types

---
+++ module1/src/1.4.ts
@@ -0,0 +1,53 @@
+{// Basic Data Type
+
+// string
+let firstName: string = "Mehedi";
+
+// Number
+let roll: number = 751962;
+
+// boolean
+let isStudent: boolean = true;
+
+// null
+let myNull: null = null;
+
+// undefined
+let myUndefined: undefined = undefined;
+
+// array
+let names: string[] = ["Mehedi", "Hasan", "Mehad"];
+let numbers: number[] = [1, 2, 3, 4, 5];
+
+// tuple
+let person: [string, number, boolean] = ["Mehedi", 24, true];
+person[0] = "Mehad";
+person[1] = 52;
+console.log(person);
+
+// object
+let user: object = {
+  firstName: "Mehedi",
+  middleName: "Hassan",
+  age: 24,
+};
+
+// object
+const myInfo :{
+    firstName: string,
+    middleName: string,
+    last: null
+    age: number,
+    isGood: boolean
+
+} ={
+    firstName: "Mehedi",
+    middleName: "Hassan",
+    last: null,
+    age: 22,
+    isGood: true,
+}
+
+
+
+}
