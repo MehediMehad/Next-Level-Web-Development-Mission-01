3915dfcd0be12045e698506412cd38f3ff488ca2
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-11-05 13:46:43 +0600

    3-2: Inheritance in OOP

---
+++ module3/3.2.ts
@@ -0,0 +1,60 @@
+{
+  // Inheritance in OOP
+
+  class Person {
+    name: string;
+    age: number;
+    address: string;
+
+    constructor(name: string, age: number, address: string) {
+      this.name = name;
+      this.age = age;
+      this.address = address;
+    }
+    getSleep(numberOfHours: number) {
+      console.log(`${this.name} will sleep for ${numberOfHours}`);
+    }
+  }
+
+  // Student
+  class Student extends Person {
+    //
+    constructor(name: string, age: number, address: string) {
+      super(name, age, address);
+    }
+  }
+
+  const student1 = new Student("Mehedi", 22, "Dhaka");
+  student1.name;
+  student1.address;
+  student1.age;
+  student1.getSleep;
+
+  // Teacher
+  class Teacher extends Person {
+    designation: string;
+
+    constructor(
+      name: string,
+      age: number,
+      address: string,
+      designation: string
+    ) {
+      super(name, age, address);
+      this.designation = designation;
+    }
+    getSleep(numberOfHours: number) {
+      console.log(`${this.name} will sleep for ${numberOfHours}`);
+    }
+    takeClass(numberOfClass: number) {
+      console.log(`${this.name} will take ${numberOfClass}`);
+    }
+  }
+
+  const teacher1 = new Teacher("Mir", 22, "Dhaka", "mentors");
+  teacher1.address;
+  teacher1.age;
+  teacher1.designation;
+  teacher1.getSleep;
+  teacher1.takeClass;
+}
